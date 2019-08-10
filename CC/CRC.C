f6bdb6346e70985767bd248cd877359bab8da006
YashJaveri <yjaveri99@gmail.com>
2019-08-10 23:19:33 +0530

    ExtraLongFactorial

--- CC/CRC.C
+++ CC/CRC.C
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+char* divide(char genFunc[], char mess[]){
+    int i=0;
+    int messLen = strlen(mess);
+    int genFunc = strlen(genFunc);
+    
+    while(i<messLen){
+        
+    }
+    return  mess;
+}
+
 int main(){
     char genFunc[] = "1011";
     char mess[] = "110101";    
@@ -10,13 +21,8 @@ int main(){
     for(int i=1; i<len; i++)
         strcat(mess, "0");    
 
-    char *rem = divide(genFunc, mess);
+    char* rem = divide(genFunc, mess);
 
-    printf(rem);
+    //printf("%s", rem);
     return 0;
-}
-
-char *divide(char genFunc[], char mess[]){
-    char *test = "Hello";
-    return  test;
 }
---
+++ CC/ExtraLongFactorial.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class ExtraLongFactorial {
+
+    // Complete the extraLongFactorials function below.
+    static void extraLongFactorials(int n) {
+        List<Integer> ans = new ArrayList<>();
+        int carry = 0, num = 0;
+
+        for (int i = 0; i < 160; i++)
+            ans.add(0);
+
+        ans.set(0, 1);
+        for (int i = 1; i <= n; i++) {
+            carry = 0;
+            for (int j = 0; j < ans.size(); j++) {
+                num = i * ans.get(j) + carry;
+                ans.set(j, num % 10);
+                carry = num / 10;
+            }
+            ans.set(ans.size() - 1, carry);
+        }
+
+        int i=ans.size();
+        for (i = ans.size() - 1; i >= 0; i--)
+            if(ans.get(i) != 0)
+                break;        
+
+        for(int j=i; j>=0; j--)
+            System.out.print(ans.get(j));
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        extraLongFactorials(n);
+
+        scanner.close();
+    }
+}
