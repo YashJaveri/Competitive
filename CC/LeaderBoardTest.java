d0e2418d6673931c165c4ff1982df786f36a0a20
YashJaveri <yjaveri99@gmail.com>
2019-08-14 16:47:54 +0530

    MaxPRod

--- CC/LeaderBoardTest.java
+++ CC/LeaderBoardTest.java
@@ -1,4 +1,3 @@
-
 //https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem
 import java.io.*;
 import java.util.*;
@@ -26,7 +25,7 @@ public class Solution {
                     for (int j = pos[i - 1]; j < scores.length; j++) {
                         System.out.println("Less: " + scores[j] + " , alice: " + alice[i]);
                         if (alice[i] >= scores[j]) {
-                            ans[i] = ranks[j];
+                            ans[i] = ranks[j];                            
                             break;
                         } else if (j == scores.length - 1)
                             ans[i] = ranks[scores.length - 1] + 1;
@@ -36,12 +35,14 @@ public class Solution {
                     for (int j = pos[i - 1]; j >= 0; j--) {
                         System.out.println("More: " + scores[j] + " , alice: " + alice[i]);
                         if (alice[i] < scores[j]) {
-                            ans[i] = ranks[j] + 1;
+                            ans[i] = ranks[j] + 1;                            
                             break;
-                        } else if (alice[i] == scores[j]) {
-                            ans[i] = ranks[j];
+                        }
+                        else if (alice[i] == scores[j]) { 
+                            ans[i] = ranks[j];                            
                             break;
-                        } else if (j == 0)
+                        }
+                        else if (j == 0)
                             ans[i] = 1;
                         pos[i] = j;
                     }
@@ -65,8 +66,7 @@ public class Solution {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        // BufferedWriter bufferedWriter = new BufferedWriter(new
-        // FileWriter(System.getenv("OUTPUT_PATH")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int scoresCount = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
@@ -76,33 +76,37 @@ public class Solution {
         String[] scoresItems = scanner.nextLine().split(" ");
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        /// System.out.println("SCores COunt Actual: " + scoresItems.length);
-        for (int i = 0; i < scoresItems.length; i++) {
+        for (int i = 0; i < scoresCount; i++) {
             int scoresItem = Integer.parseInt(scoresItems[i]);
             scores[i] = scoresItem;
         }
-        /*
-         * for (int i = 0; i < scoresCount; i++) { System.out.println(scores[i]); }
-         */
+
         int aliceCount = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         int[] alice = new int[aliceCount];
 
         String[] aliceItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        // System.out.println("AliceCount: " + aliceCount);
-        /*
-         * for (int i = 0; i < aliceCount; i++) { System.out.println(aliceItems[i]); }
-         */
-        for (int i = 0; i < aliceItems.length; i++) {
+        for (int i = 0; i < aliceCount; i++) {
             int aliceItem = Integer.parseInt(aliceItems[i]);
             alice[i] = aliceItem;
         }
 
         int[] result = climbingLeaderboard(scores, alice);
 
-        System.out.println(result);
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
 
         scanner.close();
     }
---
+++ CC/MaxProd.java
@@ -0,0 +1,58 @@
+import java.lang.*;
+import java.util.*;
+
+public class MaxProd {
+
+    public static void main(String[] args) {
+        Integer arr[] = { 5, 9, 6, 8, 6, 4, 6, 9, 5, 4, 9 };
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for (int i : arr)
+            list.add(i);
+
+        Solution sol = new Solution();
+
+        int a = sol.maxSpecialProduct(list);
+        System.out.println(a);
+    }
+}
+
+class Solution {
+    public int maxSpecialProduct(ArrayList<Integer> A) {
+        ArrayList<Integer> ls = new ArrayList<>(), rs = new ArrayList<>();
+        Stack<Integer> s = new Stack<>();
+        long max = 0, res = 0;
+
+        for (int i = 0; i < A.size(); i++) {
+            while (!s.empty() && A.get(s.peek()) <= A.get(i))
+                s.pop();
+
+            if (s.empty())
+                ls.add(0);
+            else
+                ls.add(s.peek());
+            s.push(i);
+        }
+        s.clear();
+
+        for (int i = A.size() - 1; i >= 0; i--) {
+            while (!s.empty() && A.get(s.peek()) <= A.get(i))
+                s.pop();
+
+            if (s.empty())
+                rs.add(0);
+            else
+                rs.add(s.peek());
+            s.push(i);
+        }
+
+        for (int i = 0; i < A.size(); i++) {
+            res = (long) ls.get(i) * (long) rs.get(A.size() - 1 - i);
+            if (max < res) {
+                max = res;
+            }
+        }
+
+        return (int) (max % 1000000007);
+    }
+}
